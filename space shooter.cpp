5e911490e706620911f0ee527120014176cb32c5
mingfu0708 <73901385+mingfu0708@users.noreply.github.com>
2020-12-15 15:06:37 +0800

    Update space shooter.cpp
    排版

--- space shooter.cpp
+++ space shooter.cpp
@@ -20,34 +20,36 @@ using namespace std;
 class Bullet
 {
 private:
-int x;
-int y;
+  int x;
+  int y;
 public:
-int X()  { return x; }
-int Y()  { return y; }
-Bullet(int _x, int _y)
-{
-x = _x;
-y = _y;
-}
-bool isOut()
-{
-if(y <= 3)
-{ // If the bullet reaches the top of the map
-gotoxy(x,y); printf(" "); // It disappears
-return true; // And informs the ame that it should no longer exist :c
-}
-else
-{
-return false;
-}
-}
-void Move()
-{
-gotoxy(x,y); printf(" ");
-y--;
-gotoxy(x,y); printf("."); // The shape of the bullet
-}
+  int X()  { return x; }
+  int Y()  { return y; }
+  Bullet(int _x, int _y)
+  {
+    x = _x;
+    y = _y;
+  }
+  
+  bool isOut()
+  {
+    if(y <= 3)
+    { // If the bullet reaches the top of the map
+      gotoxy(x,y); printf(" "); // It disappears
+      return true; // And informs the ame that it should no longer exist :c
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  void Move()
+  {
+    gotoxy(x,y); printf(" ");
+    y--;
+    gotoxy(x,y); printf("."); // The shape of the bullet
+  }
 };
 
 
@@ -61,7 +63,6 @@ gotoxy(x,y); printf("."); // The shape of the bullet
 
 int main()
 {
-
 HideCursor();
 WelcomeMessage();
 getch();
@@ -75,67 +76,70 @@ list<Asteroid*>::iterator asteroid;
 
 for(int i = 0; i < 10; i++) // Pick as many asteroids as you want
 { // They are randomly placed in the map but not too low
-Asteroids.push_back(new Asteroid(rand()%78 + 1, rand()%4 + 3));
+  Asteroids.push_back(new Asteroid(rand()%78 + 1, rand()%4 + 3));
 }
+  
 SpaceShip ss = SpaceShip(40,20); // Here our adventure begins
 int score = 0; // Your score :3
 
 while(!ss.isDead() && score != 100) // If you die or reach 100 points the game ends
 {
-if(kbhit())
-{
-char key = getch();
-if(key == ' ')
-{ // If you press the space bar you add a bullet to the bullet list
-Bullets.push_back(new Bullet(ss.X() + 2, ss.Y() - 1));
-}
-}
-for(bullet = Bullets.begin(); bullet != Bullets.end(); bullet++)
-{ // For every bullet that is in space
-(*bullet)->Move();
-if((*bullet)->isOut())
-{ // If the bullet reached the end of the map
-delete(*bullet); // It gets deleted
-bullet = Bullets.erase(bullet);
-}
-}
-for(asteroid = Asteroids.begin(); asteroid != Asteroids.end(); asteroid++)
-{ // Every asteroid checks if the spaceship shares it's coordinates :3
-(*asteroid)->Collision(ss);
-}
-for(asteroid = Asteroids.begin(); asteroid != Asteroids.end(); asteroid++)
-{
-for(bullet = Bullets.begin(); bullet != Bullets.end(); bullet++)
-{ // asteroid-bullet collision
-int astX = (*asteroid)->X(); //Coordinates of the asteroid
-int astY = (*asteroid)->Y();
-int bulX = (*bullet)->X(); // Coordinates of the bullet
-int bulY = (*bullet)->Y();
-if((astX == bulX) && ((astY == bulY) || (astY + 1 == bulY)))
-{ //Chances are that in the Y axis they never reach the same value, that case must be considered
-gotoxy(bulX,bulY); printf(" "); // Makes the bullet invisible
-gotoxy(astX,astY); printf("X"); printf(" "); // I still have my doubts in this part, but it tries to signal a collision, sometimes the X remains theme...
-delete(*bullet); // You delete the bullet
-bullet = Bullets.erase(bullet);
-delete(*asteroid);// And the asteroid
-asteroid = Asteroids.erase(asteroid);
-Asteroids.push_back(new Asteroid(rand()%78 + 1, rand()%4 + 3)); // in order to not reduce the number of asteroids I add one everytime one is destroyed
-score += 10; // And you get 10 points for a job well done :3
-}
-}
-}
-ss.Move();
-gotoxy(56,1); printf("%d", score);
-Sleep(30); // This is essential, otherwise the game would be unplayable
-}
+  if(kbhit())
+  {
+    char key = getch();
+    if(key == ' ')
+    { // If you press the space bar you add a bullet to the bullet list
+      Bullets.push_back(new Bullet(ss.X() + 2, ss.Y() - 1));
+    }
+  }
+  for(bullet = Bullets.begin(); bullet != Bullets.end(); bullet++)
+  { // For every bullet that is in space
+    (*bullet)->Move();
+    if((*bullet)->isOut())
+    { // If the bullet reached the end of the map
+      delete(*bullet); // It gets deleted
+      bullet = Bullets.erase(bullet);
+    }
+  }
+  for(asteroid = Asteroids.begin(); asteroid != Asteroids.end(); asteroid++)
+  { // Every asteroid checks if the spaceship shares it's coordinates :3
+    (*asteroid)->Collision(ss);
+  }
+  for(asteroid = Asteroids.begin(); asteroid != Asteroids.end(); asteroid++)
+  {
+    for(bullet = Bullets.begin(); bullet != Bullets.end(); bullet++)
+    { // asteroid-bullet collision
+      int astX = (*asteroid)->X(); //Coordinates of the asteroid
+      int astY = (*asteroid)->Y();
+      int bulX = (*bullet)->X(); // Coordinates of the bullet
+      int bulY = (*bullet)->Y();
+      if((astX == bulX) && ((astY == bulY) || (astY + 1 == bulY)))
+      { //Chances are that in the Y axis they never reach the same value, that case must be considered
+        gotoxy(bulX,bulY); printf(" "); // Makes the bullet invisible
+        gotoxy(astX,astY); printf("X"); printf(" "); // I still have my doubts in this part, but it tries to signal a collision, sometimes the X remains theme...
+        delete(*bullet); // You delete the bullet
+        bullet = Bullets.erase(bullet);
+        delete(*asteroid);// And the asteroid
+        asteroid = Asteroids.erase(asteroid);
+        Asteroids.push_back(new Asteroid(rand()%78 + 1, rand()%4 + 3)); // in order to not reduce the number of asteroids I add one everytime one is destroyed
+        score += 10; // And you get 10 points for a job well done :3
+      }
+    }
+  }
+  ss.Move();
+  gotoxy(56,1); printf("%d", score);
+  Sleep(30); // This is essential, otherwise the game would be unplayable
+}//end while
+  
 if(!ss.isDead())
 { // If you died
-GameOverVictoryMessage();
+  GameOverVictoryMessage();
 }
 else
 { // If you won
-GameOverDefeatMessage();
+  GameOverDefeatMessage();
 }
+  
 Sleep(5000);
 SpecialMessage(); // Surprise
 Sleep(5000);
